4de84fecc217b6cf8dee96afcd2fbdb4f0de55b6
Tom Bolks <tom.bolks.tb@gmail.com>
2022-11-12 23:14:28 +0100

    Added bones

--- src/main/java/universalDiscord/DeathThumbnail.java
+++ src/main/java/universalDiscord/DeathThumbnail.java
@@ -4,6 +4,7 @@ public enum DeathThumbnail {
     NONE(null),
     DEATH("https://oldschool.runescape.wiki/images/Death.png"),
     GRAVE("https://oldschool.runescape.wiki/images/Grave.png"),
+    BONES("https://oldschool.runescape.wiki/images/Bones_detail.png"),
     ANGEL_GRAVE("https://oldschool.runescape.wiki/images/Grave_%28Angel%29.png");
 
     public final String thumbnailUrl;
